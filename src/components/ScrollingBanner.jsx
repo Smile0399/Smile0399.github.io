78584d7b5155ff410d366aa9f93b4dfe3b9d8c19
Zane Campbell <zanegitinit@gmail.com>
2023-06-24 15:32:47 -0700

    added image to scrolling banner

--- src/components/ScrollingBanner.jsx
+++ src/components/ScrollingBanner.jsx
@@ -5,16 +5,19 @@ const ScrollingBanner = () => {
     '3.jpg',
     '4.jpg',
     '5.jpg',
+    '6.jpg',
     '1.jpg',
     '2.jpg',
     '3.jpg',
     '4.jpg',
     '5.jpg',
+    '6.jpg',
     '1.jpg',
     '2.jpg',
     '3.jpg',
     '4.jpg',
-    '5.jpg'
+    '5.jpg',
+    '6.jpg'
   ]
 
   return (
